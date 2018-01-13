56d8766d193524b8aad580fbbb4252acd9d3e576
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-01-13 16:25:26 -0600

    added some styling

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 console.log("scripts.js");
 
+//variables for divs, buttons, and inputs
 let fortune = document.getElementById("fortune");
 
 let submitBtn = document.getElementById("submit");
+let resetBtn = document.getElementById("reset");
 
 let location1 = document.getElementById("location1");
 let location2 = document.getElementById("location2");
@@ -16,22 +18,16 @@ let pet1 = document.getElementById("pet1");
 let pet2 = document.getElementById("pet2");
 let pet3 = document.getElementById("pet3");
 
+//holding inputs
 let locations = [];
 let jobs = [];
 let pets = [];
 let mash = ['mansion', 'apartment', 'shack', 'house'];
 
-
-let randomNum = (Math.floor(Math.random() * 4));
-//console.log("random num between 0 - 3", randomNum);
-//console.log("mash", mash[randomNum]);
-dwelling = mash[randomNum];
-
-let random2 = (Math.floor(Math.random() * 3));
+//random num between 0 - 2. For location, job, pet selection.
+let randomNum2 = (Math.floor(Math.random() * 3));
 //location = locations[randomNum];
 //console.log("rand location", location);
-//job = jobs[randomNum];
-//pet = pets[randomNum];
 
 submitBtn.addEventListener("click", submit);
 
@@ -51,10 +47,35 @@ function submit () {
 	p3 = pet3.value;
 	pets.push(p1, p2, p3);
 
-	console.log("locations: ", locations);
-	console.log("pets: ", pets);
-	console.log("jobs: ", jobs);
+	//generate random num between 0 - 3. For mash selection.
+	//inside function so that this will run everty time the submit button is pressed
+	let randomNum = (Math.floor(Math.random() * 4));
+	let dwelling = mash[randomNum];
 
-fortune.innerHTML = `<p>You will live in a <b>${dwelling}</b>, located in <b>${locations[random2]}</b>. You will be a <b>${jobs[random2]}</b> who owns a <b>${pets[random2]}</b>.</p>`;
+	//console.log("random num between 0 - 3", randomNum);
+	//console.log("mash", mash[randomNum]);
+	
+
+	fortune.innerHTML = `<p>You will live in a <b>${dwelling}</b>, located in <b>${locations[randomNum2]}</b>. You will be a <b>${jobs[randomNum2]}</b> who owns a <b>${pets[randomNum2]}</b>.</p>`;
 };
 
+resetBtn.addEventListener("click", reset);
+
+function reset () {
+	location1.value = "";
+	location2.value = "";
+	location3.value = "";
+
+	job1.value = "";
+	job2.value = "";
+	job3.value = "";
+
+	pet1.value = "";
+	pet2.value = "";
+	pet3.value = "";
+
+	locations = [];
+	jobs = [];
+	pets = [];
+	fortune.innerHTML = "";
+};
